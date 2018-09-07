8580f3fcf8ddc41af450ecb4a68b1b3cf6ee0f06
Ed Spry <ed.spry@ericsson.com>
2018-09-07 17:50:13 -0500

    S5 L98

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <AppHeader></AppHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import { mapActions } from 'vuex';
+    import AppHeader from './components/AppHeader';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: {AppHeader},
+        methods: mapActions(['login'])
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
